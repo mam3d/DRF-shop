db9772ccbdd08511919325260644554a42fb0efc
mam3d <immam3d@gmail.com>
2021-11-14 14:52:52 +0330

    edited validatephone view and add validation on serializer

--- user/api/serializers.py
+++ user/api/serializers.py
@@ -14,6 +14,12 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ["phone","password","password2"]
 
+    def validate_phone(self,value):
+        user = CustomUser.objects.get(phone=value)
+        if user:
+            raise serializers.ValidationError("this phone number already taken")
+        return value
+
     def validate(self,data):
         password = data.get("password")
         password2 = data.get("password2")
@@ -21,6 +27,11 @@ class UserRegisterSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("password's didn't match",code="Not matched")
         return data
 
+    def create(self,validated_data):
+        user = CustomUser(phone=validated_data.get("phone"))
+        user.set_password(validated_data.get("password"))
+        user.save()
+        return user
 
 
 
@@ -30,3 +41,11 @@ class PhoneSerializer(serializers.Serializer):
             return value
         raise serializers.ValidationError("please enter correct format ex:0912***2027")
     phone = serializers.CharField(validators=[regex_validator])
+
+
+    def validate_phone(self,value):
+        user = CustomUser.objects.get(phone=value)
+        if user:
+            raise serializers.ValidationError("this phone number already taken")
+        return value
+        
--- user/api/views.py
+++ user/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from ..models import CustomUser, PhoneOtp
 import random
-from .serializers import PhoneSerializer
+from .serializers import PhoneSerializer, UserRegisterSerializer
 from kavenegar import *
 
 
@@ -24,12 +24,6 @@ class ValidatePhone(APIView):
         serializer = PhoneSerializer(data=request.data)
         if serializer.is_valid():
             phone = serializer.validated_data.get("phone")
-            user_qs = CustomUser.objects.filter(phone=phone)
-            if user_qs:
-                return Response(
-                    {"error":"user with this phone number already exist"},
-                    status=status.HTTP_400_BAD_REQUEST)
-                    
             otp_qs = PhoneOtp.objects.filter(phone=phone)
             if otp_qs.exists():
                 otp = otp_qs[0]
@@ -42,7 +36,7 @@ class ValidatePhone(APIView):
                 otp.code = random.randint(9999,99999)
                 otp.counter += 1
                 otp.save()
-                # send_sms(otp.code,otp.phone)
+                send_sms(otp.code,otp.phone)
                 return Response(
                     {"success":"the code has been successfully sent to your phone"},
                     status = status.HTTP_200_OK
@@ -50,7 +44,7 @@ class ValidatePhone(APIView):
 
             else:
                 otp = PhoneOtp.objects.create(phone=phone,code=random.randint(9999,99999))
-                # send_sms(otp.code,otp.phone)
+                send_sms(otp.code,otp.phone)
                 return Response(
                     {"success":"the code has been successfully sent to your phone"},
                     status = status.HTTP_200_OK
@@ -59,3 +53,22 @@ class ValidatePhone(APIView):
 
 
 
+
+class RegisterView(APIView):
+    def post(self,request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            phone = serializer.validated_data.get("phone")
+            phoneot_qs = PhoneOtp.objects.filter(phone=phone)
+            if phoneot_qs.exists():
+                phoneotp = phoneot_qs[0]
+                code = request.data.get("code")
+                if phoneotp.code == int(code):
+                    serializer.save()
+                    phoneotp.delete()
+                    phoneotp.save()
+                    return Response(serializer.data,status=status.HTTP_201_CREATED)
+                else:
+                    return Response({"failed":"wrong code"},status=status.HTTP_400_BAD_REQUEST)
+            return Response({"failed":"you have to verify your number first"},status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
