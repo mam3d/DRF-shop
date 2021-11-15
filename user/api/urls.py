b161de2b203cd0e035139862f5738eace0bfada0
mam3d <immam3d@gmail.com>
2021-11-15 11:31:42 +0330

    added logout view

--- user/api/urls.py
+++ user/api/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
 from .views import ValidatePhone,RegisterView, LoginView
+from knox.views import LogoutView
 
 urlpatterns = [
     path("validate-phone/",ValidatePhone.as_view(),name="validate-phone"),
     path("register/",RegisterView.as_view(),name="register"),
     path("login/",LoginView.as_view(),name="login"),
+    path("logout/", LogoutView.as_view(), name='knox_logout'),
 ]
