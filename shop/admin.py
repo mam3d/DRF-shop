3250df6503a9a723dd3825cab6dbf45d50a097de
mam3d <immam3d@gmail.com>
2021-11-16 16:47:00 +0330

    changed order admin to show orderitems

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Category, Order, OrderItem, Product
 
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    fields = ["product","quantity","total_product_price"]
+    readonly_fields = ["product","quantity","total_product_price"]
+    extra = 0
+
+
 @admin.register(Category)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["name","slug"]
@@ -17,6 +24,7 @@ class ProductAdmin(admin.ModelAdmin):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ["user","date_ordered","is_ordered","total_order_price"]
     list_filter = ["date_ordered","is_ordered"]
+    inlines = [OrderItemInline]
 
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
