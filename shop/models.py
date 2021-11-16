7950d2b5dd4a939f1ddf498e595c58b19aa373ae
mam3d <immam3d@gmail.com>
2021-11-16 10:38:23 +0330

    add category table

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+ 
+    class Meta:
+        verbose_name_plural = "categories"
