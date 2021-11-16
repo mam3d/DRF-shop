73313ff52049dada5b479821d66e8c6f9decf323
mam3d <immam3d@gmail.com>
2021-11-16 17:18:43 +0330

    productadd view

--- shop/api/urls.py
+++ shop/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import (CategoryListView,CategoryDetailView,
                         ProductListView,ProductDetailView,
-                        
+                        ProductAdd,
                         )
 urlpatterns = [
     path("categories/",CategoryListView.as_view(),name="categories"),
     path("category/<slug:slug>/",CategoryDetailView.as_view(),name="category"),
     path("products/",ProductListView.as_view(),name="products"),
     path("product/<slug:slug>/",ProductDetailView.as_view(),name="product"),
+    path("product/<int:id>/add/",ProductAdd.as_view(),name="product-add"),
 ]
--- shop/api/views.py
+++ shop/api/views.py
@@ -1,6 +1,10 @@
-from typing import FrozenSet
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import generics
-from shop.models import Category, Product
+from shop.models import Category, Order, OrderItem, Product
 from .serializers import (
     CategoryDetailSerializer,CategorySerializer,ProductSerializer
 )
@@ -23,4 +27,18 @@ class ProductDetailView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "slug"
+
+
+class ProductAdd(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self,request,id):
+        product = get_object_or_404(Product,id=id)
+        order,created = Order.objects.get_or_create(user=request.user,is_ordered=False)
+        orderitem,created = OrderItem.objects.get_or_create(user=request.user,product=product,order=order)
+        if created:
+            return Response({"product":product.name,"successfull":"product added to your cart"},status=status.HTTP_201_CREATED)
+        else:
+            orderitem.quantity += 1
+            orderitem.save()
+            return Response({"product":product.name,"successfull":"your product's quantity has been increased"},status=status.HTTP_201_CREATED)
 
