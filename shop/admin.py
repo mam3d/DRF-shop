8cd7d5a7f95a02dc9a2d6d30dcd70dc195be92b4
mam3d <immam3d@gmail.com>
2021-11-16 11:02:44 +0330

    add Product Table

--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Product
 
 admin.site.register(Category)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name","price","category","is_available","availability","slug"]
+    list_editable = ["price","category","is_available","availability","slug"]
+    list_filter = ["category","is_available"]
+    prepopulated_fields = ({"slug":("name",)})
--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -9,3 +9,26 @@ class Category(models.Model):
  
     class Meta:
         verbose_name_plural = "categories"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True,null=True)
+    image = models.ImageField(blank=True,null=True)
+    price = models.PositiveIntegerField()
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,blank=True,null=True)
+    is_available = models.BooleanField(default=True)
+    availability = models.IntegerField()
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
+    def save(self,*args, **kwargs):
+        if self.availability < 1:
+            self.is_available = False
+        if not self.slug:
+            self.slug = slugify(self.name)
+        
+        return super().save(*args, **kwargs)
