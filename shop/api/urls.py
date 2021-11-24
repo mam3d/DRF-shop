1da0cfa10920a5b4d881c9d32677a784c88b176d
mam3d <immam3d@gmail.com>
2021-11-24 14:35:35 +0330

    checkout view with idpay api

--- shop/api/urls.py
+++ shop/api/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views import (CategoryListView,CategoryDetailView,
                         ProductListView,ProductDetailView,
                         ProductAdd,ProductRemove,Cart,
-                        AddDiscount,
+                        AddDiscount,CheckoutView,VerifyView
                         )
 urlpatterns = [
     path("categories/",CategoryListView.as_view(),name="categories"),
     path("category/<slug:slug>/",CategoryDetailView.as_view(),name="category"),
     path("cart/",Cart.as_view(),name="cart"),
+    path("checkout/",CheckoutView.as_view(),name="checkout"),
+    path("verify/",VerifyView.as_view(),name="verify"),
     path("discount/",AddDiscount.as_view(),name="add-discount"),
     path("products/",ProductListView.as_view(),name="products"),
     path("product/<slug:slug>/",ProductDetailView.as_view(),name="product"),
